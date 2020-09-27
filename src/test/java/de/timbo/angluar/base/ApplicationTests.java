b1db8f588832d1a1a8d7d678315ef78e791be75d
Tim Börner <timboerner@web.de>
2020-09-27 19:46:55 +0200

    Initial commit

---
+++ src/test/java/de/timbo/angluar/base/ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.timbo.angluar.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
